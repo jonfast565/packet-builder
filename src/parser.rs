0bfd4557f1a56d6d43dff0666c6db57a47712c25
Jon Fast <jnfstdj656@gmail.com>
2021-06-28 17:05:13 -0400

    caused issues...

--- src/parser.rs
+++ src/parser.rs
@@ -6,7 +6,7 @@ use pest::Parser;
 pub struct PacketParser2;
 
 pub fn parse_file(input: &String) -> Result<String, Error<Rule>> {
-    let packet = PacketParser2::parse(Rule::Packets, input)?.next().unwrap();
+    let packet = PacketParser2::parse(Rule::packets, input)?.next().unwrap();
     dbg!(packet);
     Ok(String::new())
 }
