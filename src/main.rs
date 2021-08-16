638eb4f29a240fbea2ad0d4ad0f4e082df4d8575
Jon Fast <jnfstdj656@gmail.com>
2021-08-16 15:24:50 -0400

    updated to use all gens

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,25 @@ mod parser;
 mod utilities;
 mod generators;
 
-// use crate::generators::c_generator::CGenerator;
-// use crate::generators::zig_generator::ZigGenerator;
+use crate::generators::c_generator::CGenerator;
+use crate::generators::zig_generator::ZigGenerator;
 use crate::generators::rust_generator::RustGenerator;
 
 use std::fs;
 
 fn main() {
     println!("--- Packet Builder ---");
+    let result_type = "rust";
     let file =
         fs::read_to_string("./test_packet.packet").expect("Something went wrong reading the file");
     let contents = parser::parse_file(&file).unwrap();
     for packet in contents {
-        // let result = CGenerator::generate(&packet);
-        // println!("{}", result);
-        // let result2 = ZigGenerator::generate(&packet);
-        // println!("{}", result2);
-        let result3 = RustGenerator::generate(&packet);
-        println!("{}", result3);
+        let packet_result = match result_type {
+            "c" => CGenerator::generate(&packet),
+            "rust" => RustGenerator::generate(&packet),
+            "zig" => ZigGenerator::generate(&packet),
+            _ => String::new()
+        };
+        println!("{}", packet_result);
     }
 }
