4dd3ba5fca6d939bd7888763fd43424ce8428f7e
Jon Fast <jnfstdj656@gmail.com>
2021-08-20 16:02:53 -0400

    slight grammar changes

--- src/models/parsing_models.rs
+++ src/models/parsing_models.rs
@@ -33,7 +33,7 @@ pub enum ExprNode {
     SumOf(Box<ExprNode>),
     ProductOf(Box<ExprNode>),
     ActivationRecord(String, Vec<ExprNode>),
-    NoExpr
+    NoExpr,
 }
 
 impl ExprNode {
@@ -55,65 +55,45 @@ impl ExprNode {
     }
     pub fn get_length_bytes(&self) -> usize {
         match self {
-            ExprNode::UnsignedInteger8(y) => {
-                match y {
-                    Some(n) => *n,
-                    None => 1
-                }
-            }
-            ExprNode::Integer8(y) => {
-                match y {
-                    Some(n) => *n,
-                    None => 1
-                }
-            }
-            ExprNode::UnsignedInteger16(y) => {
-                match y {
-                    Some(n) => n * 2,
-                    None => 2
-                }
-            }
-            ExprNode::Integer16(y) => {
-                match y {
-                    Some(n) => n * 2,
-                    None => 2
-                }
-            }
-            ExprNode::UnsignedInteger32(y) => {
-                match y {
-                    Some(n) => n * 4,
-                    None => 4
-                }
-            }
-            ExprNode::Integer32(y) => {
-                match y {
-                    Some(n) => n * 4,
-                    None => 4
-                }
-            }
-            ExprNode::UnsignedInteger64(y) => {
-                match y {
-                    Some(n) => n * 8,
-                    None => 8
-                }
-            }
-            ExprNode::Integer64(y) => {
-                match y {
-                    Some(n) => n * 8,
-                    None => 8
-                }
-            }
-            ExprNode::Float32(y) => {
-                match y {
-                    Some(n) => n * 4,
-                    None => 4
-                }
-            }
-            ExprNode::Float64(y) => {
-                match y {
-                    Some(n) => n * 8,
-                    None => 8
-                }
+            ExprNode::UnsignedInteger8(y) => match y {
+                Some(n) => *n,
+                None => 1,
+            },
+            ExprNode::Integer8(y) => match y {
+                Some(n) => *n,
+                None => 1,
+            },
+            ExprNode::UnsignedInteger16(y) => match y {
+                Some(n) => n * 2,
+                None => 2,
+            },
+            ExprNode::Integer16(y) => match y {
+                Some(n) => n * 2,
+                None => 2,
+            },
+            ExprNode::UnsignedInteger32(y) => match y {
+                Some(n) => n * 4,
+                None => 4,
+            },
+            ExprNode::Integer32(y) => match y {
+                Some(n) => n * 4,
+                None => 4,
+            },
+            ExprNode::UnsignedInteger64(y) => match y {
+                Some(n) => n * 8,
+                None => 8,
+            },
+            ExprNode::Integer64(y) => match y {
+                Some(n) => n * 8,
+                None => 8,
+            },
+            ExprNode::Float32(y) => match y {
+                Some(n) => n * 4,
+                None => 4,
+            },
+            ExprNode::Float64(y) => match y {
+                Some(n) => n * 8,
+                None => 8,
             },
             ExprNode::MacAddress => 6,
             _ => 0,
